d112a78e52c2f80c815dc04f34cc1ac9e52ed7ec
YourtionGuo <yourtion@gmail.com>
2018-01-10 15:23:03 +0800

    simple components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create the ApolloClient instance 
+const SERVICE_ID = 'cjc8qcgbj1bxm0175g4fnf3xd';
+const client = new ApolloClient({
+  link: new HttpLink('https://api.graph.cool/simple/v1/' + SERVICE_ID),
+  cache: new InMemoryCache(),
+});
+
+// Create a higher-order component to wrap the App component
+const withApolloProvider = Comp => (
+  <ApolloProvider client={client}>{Comp}</ApolloProvider>
+);
+
+ReactDOM.render(
+    withApolloProvider(<App />),
+    document.getElementById('root')
+);
+
 registerServiceWorker();
